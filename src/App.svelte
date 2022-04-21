f8f31d01a6ea34171e95b70018a26e4c449539ce
DANCHE <dangdancheg@gmail.com>
2022-04-21 22:48:47 +0300

    making variables in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
-  export let name;
+  let name = "dancheg";
+  let antoherVar = "another variable";
 </script>
 
 <main>
   <h1>Hello {name}!</h1>
   <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
+    {antoherVar}
   </p>
 </main>
 
