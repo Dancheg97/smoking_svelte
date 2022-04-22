5a757e58776166265765b1b536ff089851f24cd8
DANCHE <dangdancheg@gmail.com>
2022-04-22 08:11:18 +0300

    reactiva variables

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,14 @@
   let fname = "jimmy";
   let sname = "hendrix";
   let ncolor = "gray";
+
+  $: fullName = `${fname} ${sname}`;
 </script>
 
 <main>
-  <h1 style="color:{ncolor}">FName: {fname}, SName {sname}</h1>
+  <h1 style="color:{ncolor}">Hello {fullName}!</h1>
+  <input type="text" bind:value={fname} />
+  <input type="text" bind:value={sname} />
   <input type="text" bind:value={ncolor} />
 </main>
 
